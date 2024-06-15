6b36160d54580819fbbc5047555016f9ca5227ab
Leonardo Vaia <leovaia32000@gmail.com>
2024-06-15 17:59:11 +0200

    Added: Automated_Pipeline.ipynb, used to scan for new data and train models automatically, Added: utils.py, cointaing useful methods for this project purposes, Feature: added the save feature to Marta's notebook

---
+++ notebooks/utils.py
@@ -0,0 +1,42 @@
+import pickle
+import pandas as pd
+from sklearn.metrics import r2_score, mean_absolute_error
+
+def save_model(model, model_name,preferred_preprocessing=0):
+    # preferred_preprocessing: 
+    # 0 for standard preprocessing preferring boolean and numerical values
+    # 1 for preprocessing preferring boolean and categorical values, trees and boosting models
+
+
+    model_dict = { 'model': model, 'model_name': model_name , 'preferred_preprocessing': preferred_preprocessing}
+    model_path = '../history/current_models/'
+    old_model_path = '../history/old_models/'
+    # if there is a file with the same name, move it to the old folder without overwriting the old models 
+    try:
+        with open(model_path + model_name + '.pkl', 'rb') as f:
+            old_model = pickle.load(f)
+        with open(old_model_path + model_name + '.pkl', 'wb') as f:
+            pickle.dump(old_model, f)
+    except:
+        pass
+
+    with open(model_path + model_name + '.pkl', 'wb') as f:
+        pickle.dump(model_dict, f)
+
+def save_scores(y_test, y_pred, model_name):
+    scores = { 'r2_score': r2_score(y_test, y_pred), 'mae': mean_absolute_error(y_test, y_pred) }
+    scores_path = '../history/current_scores/'
+    old_scores_path = '../history/old_scores/'
+
+    print(model_name)
+    print(scores)
+    # if there is a file with the same name, move it to the old folder without overwriting the old scores
+    try:
+        with open(scores_path + model_name + '.csv', 'r') as f:
+            old_scores = pd.read_csv(f)
+        old_scores.to_csv(old_scores_path + model_name + '.csv', index=False)
+    except:
+        pass
+
+    # save the scores to disk as csv
+    pd.DataFrame(scores, index=[0]).to_csv(scores_path + model_name + '.csv', index=False)
