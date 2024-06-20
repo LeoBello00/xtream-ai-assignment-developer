affafe4193bbd2719f21d07592d6a8f6b5362311
Leonardo Vaia <leovaia32000@gmail.com>
2024-06-20 19:49:04 +0200

    Feature: using notebook to explain the process

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,3 +1,4 @@
+import datetime
 import pickle
 import pandas as pd
 from sklearn.metrics import r2_score, mean_absolute_error
@@ -6,7 +7,9 @@ def save_model(model, model_name,preferred_preprocessing=0):
     # preferred_preprocessing: 
     # 0 for standard preprocessing preferring boolean and numerical values
     # 1 for preprocessing preferring boolean and categorical values, trees and boosting models
-
+    
+    #create variable containg the timestamp in form of string, including date_time
+    timestamp = datetime.datetime.now().strftime("%d%m%Y_%H%M")
 
     model_dict = { 'model': model, 'model_name': model_name , 'preferred_preprocessing': preferred_preprocessing}
     model_path = '../history/current_models/'
@@ -15,7 +18,7 @@ def save_model(model, model_name,preferred_preprocessing=0):
     try:
         with open(model_path + model_name + '.pkl', 'rb') as f:
             old_model = pickle.load(f)
-        with open(old_model_path + model_name + '.pkl', 'wb') as f:
+        with open(old_model_path + model_name + timestamp + '.pkl', 'wb') as f:
             pickle.dump(old_model, f)
     except:
         pass
@@ -24,9 +27,10 @@ def save_model(model, model_name,preferred_preprocessing=0):
         pickle.dump(model_dict, f)
 
 def save_scores(y_test, y_pred, model_name):
-    scores = { 'r2_score': r2_score(y_test, y_pred), 'mae': mean_absolute_error(y_test, y_pred) }
+    scores = { 'Model name:' : model_name,'r2_score': r2_score(y_test, y_pred), 'mae': mean_absolute_error(y_test, y_pred) }
     scores_path = '../history/current_scores/'
     old_scores_path = '../history/old_scores/'
+    timestamp = datetime.datetime.now().strftime("%d%m%Y_%H%M")
 
     print(model_name)
     print(scores)
@@ -34,7 +38,7 @@ def save_scores(y_test, y_pred, model_name):
     try:
         with open(scores_path + model_name + '.csv', 'r') as f:
             old_scores = pd.read_csv(f)
-        old_scores.to_csv(old_scores_path + model_name + '.csv', index=False)
+        old_scores.to_csv(old_scores_path + model_name + timestamp +'.csv', index=False)
     except:
         pass
 
